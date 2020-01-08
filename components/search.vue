76eecb6fe626ece03a82c13544d18f2ca5042c72
aukdme <xl78042@163.com>
2020-01-09 01:28:32 +0800

    init

---
+++ components/search.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="search-form round">
+		<text class="cuIcon-search"></text>
+		<input :adjust-position="false" type="text" placeholder="搜索图片、文章、视频" confirm-type="search"></input>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="page-container">
+		<cu-custom 
+			bg-color="bg-gradual-blue" 
+			is-back>
+			<block slot="content">
+				<search />
+			</block>
+		</cu-custom>
+	</view>
+</template>
+
+<script>
+import Search from '../../components/search.vue'
+export default {
+	components: {
+		Search
+	},
+	data() {
+		return {
+			title: 'Hello'
+		}
+	},
+	onLoad() {
+
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<style lang="scss">
+.page-container {
+	.custom-nav {
+		
+	}
+}
+</style>
