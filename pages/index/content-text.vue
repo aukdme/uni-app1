3b64cd104d835f26b26bab715103878b8cdf1ba8
aukdme <xl78042@163.com>
2020-01-09 13:03:25 +0800

    add

---
+++ pages/index/content-text.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="content-text">
+		<view class="name">中山职校基础学校</view>
+		<view class="detail-list">			
+			<view class="detail">
+				<text class="label">获得学历</text>
+				<text class="label-info">公办</text>
+			</view><view class="detail">
+				<text class="label">占地面积</text>
+				<text class="label-info">2138.43亩</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+.content-text {
+	padding: 26upx 20upx;
+	background: linear-gradient(136deg,rgba(121,81,196,1) 0%,rgba(104,101,255,1) 100%);
+	border-radius: 8upx;
+	.name {
+		height: 44upx;
+		font-size: 32upx;
+		line-height: 44upx;
+		font-weight: bold;
+		color: #fff;
+	}
+	.detail-list {
+		overflow: hidden;
+	}
+	.detail {
+		float: left;
+		margin: 10upx;
+		&:nth-child(even) {
+			margin-right: 0
+		}
+		.label {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			margin-right: 32upx;
+			width: 96upx;
+			height: 36upx;
+			font-size: 20upx;
+			color: #6B63F7;
+			background: rgba(193,187,255,1);
+			border-radius: 4upx;
+			&-info {
+				font-size: 28upx;
+				color: #D9D7FF;
+			}
+		}
+	}
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,55 +7,64 @@
 				slot="content"
 				@input="onInputSearch"/>
 		</cu-custom>
+		<view class="page-content-header">
+			<scroll-view
+				scroll-x 
+				class="bg-white nav" 
+				scroll-with-animation 
+				:scroll-left="tab.scrollLeft">
+				<view 
+					v-for="(item,index) of tab.list" 
+					@tap="tabSelect"
+					:key="index" 
+					:class="['cu-item',{'cu-item-active':index===tab.curIndex}]"
+					:data-id="index"
+					:data-name="item.name">{{item.label}}</view>
+			</scroll-view>
+		</view>
 		
-		<scroll-view 
-			scroll-x 
-			class="bg-white nav" 
-			scroll-with-animation 
-			:scroll-left="tab.scrollLeft">
-			<view 
-				v-for="(item,index) of tab.list" 
-				@tap="tabSelect"
-				:key="index" 
-				:class="['cu-item',{'cu-item-active':index===tab.curIndex}]"
-				:data-id="index"
-				:data-name="item.name">{{item.label}}</view>
-		</scroll-view>
-		
-		<swiper 
-			class="screen-swiper" 
-			:class="dotStyle?'square-dot':'round-dot'" 
-			indicator-dots 
-			circular
-			autoplay 
-			interval="5000" 
-			duration="500">
-			<swiper-item 
-				v-for="(item,index) in swiperList" 
-				:key="index">
-				<image 
-					:src="item.url" 
-					mode="aspectFill" 
-					v-if="item.type=='image'"></image>
-				<video 
-					:src="item.url" 
+		<view class="page-content-main">
+			<view class="main-block">	
+				<swiper 
+					class="screen-swiper" 
+					:class="dotStyle?'square-dot':'round-dot'" 
+					indicator-dots 
+					circular
 					autoplay 
-					loop 
-					muted 
-					:show-play-btn="false" 
-					:controls="false" 
-					objectFit="cover" 
-					v-if="item.type=='video'"></video>
-			</swiper-item>
-		</swiper>
+					interval="5000" 
+					duration="500">
+					<swiper-item 
+						v-for="(item,index) in swiperList" 
+						:key="index">
+						<image 
+							:src="item.url" 
+							mode="aspectFill" 
+							v-if="item.type=='image'"></image>
+						<video 
+							:src="item.url" 
+							autoplay 
+							loop 
+							muted 
+							:show-play-btn="false" 
+							:controls="false" 
+							objectFit="cover" 
+							v-if="item.type=='video'"></video>
+					</swiper-item>
+				</swiper>
+			</view>
+
+			<content-text class="main-block" />
+		</view>
 	</view>
 </template>
 
 <script>
 import Search from '../../components/search.vue'
+import ContentText from './content-text.vue'
 export default {
 	components: {
-		Search
+		Search,
+		ContentText
 	},
 	data() {
 		return {
@@ -125,22 +134,42 @@ export default {
 
 <style lang="scss">
 $bg-color: linear-gradient(180deg,rgba(172,40,40,1) 0%,rgba(178,35,35,1) 100%);
-.page-container {
-	.nav-bg {
-		background: $bg-color;
-	}
-	.nav {
-		display: flex;
-		background: $bg-color;
-	}
-	.cu-item {
-		height: 76upx;
-		font-size: 32upx;
-		color: #F5A8A8;
-		&-active {
-			font-size: 40upx;
-			color: #FFB200;
+%block-style {
+	overflow: hidden;
+	padding: 18upx;
+	background: #fff;
+	box-shadow: 0 4upx 4upx 0 rgba(0,0,0,0.28);
+	border-radius: 8upx;
+}
+.page {
+	&-container {
+		.nav-bg {
+			background: $bg-color;
 		}
 	}
+	
+	&-content-header {
+		.nav {
+			display: flex;
+			background: $bg-color;
+		}
+		.cu-item {
+			height: 76upx;
+			font-size: 32upx;
+			color: #F5A8A8;
+			&-active {
+				font-size: 40upx;
+				color: #FFB200;
+			}
+		}
+	}
+	
+	&-content-main {
+		padding: 16upx;
+		.main-block {
+			@extend %block-style;
+		}
+	}
+	
 }
 </style>
